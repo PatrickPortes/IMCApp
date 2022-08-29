4027db501c7e9f0d76e14df34dc081403be6c562
Patrick Portes <89034086+PatrickPortes@users.noreply.github.com>
2022-08-29 17:04:16 -0300

    Linear Layout
    Trabalhando os Conceitos de Layout, finalização do Linear Layout, com a implementação de seus Atributos de Views e suas respectivas Configurações e Design

--- app/src/main/java/com/example/imcapp/ResultActivity.kt
+++ app/src/main/java/com/example/imcapp/ResultActivity.kt
@@ -3,9 +3,9 @@ package com.example.imcapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_linear)
+        setContentView(R.layout.activity_result)
     }
 }
