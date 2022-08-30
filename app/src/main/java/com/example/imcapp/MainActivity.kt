747ffd308db0c0a886c1e47d3ecf5d0b0d04221a
Patrick Portes <89034086+PatrickPortes@users.noreply.github.com>
2022-08-30 16:09:31 -0300

    Constraint Layout
    Trabalhando os Conceitos de Layout, finalização do Constraint Layout, com a implementação de seus Atributos de Views e suas respectivas Configurações e Design, e Implementação do Plugin do Kotlin para viewBinding

--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.imcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.main_constraint.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_linear)
+        setContentView(R.layout.main_constraint)
     }
+
 }
