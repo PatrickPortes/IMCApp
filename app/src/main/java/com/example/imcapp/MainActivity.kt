c33f3d5af6cff5b71baca57898aaedfe5a17cac5
Patrick Portes <89034086+PatrickPortes@users.noreply.github.com>
2022-08-30 16:13:07 -0300

    Calcular IMC
    Implementando e Finalizando o Listener do Botão Com o Método Principal do App para Calcular IMC e Validar Campos.

--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -11,4 +11,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_constraint)
     }
 
+    fun calcular(view: View){
+
+        val peso = editTextPesoConstraint.text.toString()
+        val altura = editTextAlturaConstraint.text.toString()
+
+        val validaCampos = validarCampos(peso, altura)
+
+        if (validaCampos){
+            calcularIMC(peso, altura)
+        } else{
+            textViewTitleConstraint.text = "Preencha os Campos Abaixo Primeiro!!!"
+        }
+    }
+
+    private fun validarCampos(peso: String, altura: String): Boolean{
+
+        var camposValidados: Boolean = true
+
+        if(peso == null || peso.equals("")){
+            camposValidados = false
+        } else if( altura == null || altura.equals("")){
+            camposValidados = false
+        }
+
+        return camposValidados
+    }
+
+    private  fun calcularIMC(peso: String, altura: String){
+
+        val valorPeso = peso.toDouble()
+        val valorAltura = altura.toDouble()
+
+        val imc = valorPeso / (valorAltura * valorAltura)
+        textViewTitleConstraint.text = "Seu IMC é: $imc"
+
+    }
+
 }
